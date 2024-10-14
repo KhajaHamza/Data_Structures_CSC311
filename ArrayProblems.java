82c312b3ffbd56e80895c76ea488ed8ef6f4404e
KhajaHamza <78339659+KhajaHamza@users.noreply.github.com>
2024-10-13 23:41:56 -0700

    Add files via upload

---
+++ ArrayProblems.java
@@ -0,0 +1,39 @@
+
+public class ArrayProblems {
+
+	/*
+	Given an array of integers nums, sort the array in ascending order.
+
+	Example 1:
+	Input: nums = [5,2,3,1]
+	Output: [1,2,3,5]
+	
+	Example 2:
+	Input: nums = [5,1,1,2,0,0]
+	Output: [0,0,1,1,2,5]
+	*/
+	
+	public static int[] sortArray(int[] nums) {
+    	//TODO:finish this method. 
+		
+    	//TODO: Modify this line to return correct data.
+		return null;
+
+	}
+
+	/*
+	 * Find the kth largest element in an unsorted array. 
+	 * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+	   Example1: Input: [3,2,1,5,6,4] and k = 2
+			    Output: 5
+	   Example2: Input: [3,2,3,1,2,4,5,5,6] and k = 4
+			    Output: 4
+	*/
+    public static int findKthLargest(int[] nums, int k) {
+    	//TODO:finish this method. 
+		
+    	//TODO: Modify this line to return correct data.
+        return 0;
+    }
+    
+}
